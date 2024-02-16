2b55167fef8e314e9d857290d641b95a8912213b
sqworec <shpak1352@gmail.com>
2024-02-16 11:22:39 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import MainPage from "./pages/MainPage";
+import Background from "./components/Background";
+import ProductsPage from "./pages/ProductsPage";
+import ContactsPage from "./pages/ContactsPage";
+import AboutPage from "./pages/AboutPage";
+import Header from "./components/Header";
+
+function App() {
+    return (
+        <>
+            <Background/>
+            <Header/>
+            <Router>
+                <div className="w-full">
+                    <Routes>
+                        <Route path="/" element={<MainPage/>}/>
+                        <Route path="/products" element={<ProductsPage/>}/>
+                        <Route path="/contacts" element={<ContactsPage/>}/>
+                        <Route path="/about" element={<AboutPage/>}/>
+                    </Routes>
+                </div>
+            </Router>
+        </>
+    );
+}
+
+export default App;
