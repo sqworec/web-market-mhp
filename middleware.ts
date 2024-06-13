ae12b5c6fc0365ac0789cfbeb5fa2a6708616d7a
sqworec <shpak1352@gmail.com>
2024-06-13 15:17:06 +0300

    fin

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,18 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import {adminRoutes, apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes} from "@/routes";
-import {useSession} from "next-auth/react";
 
 const {auth} = NextAuth(authConfig)
 
-export default auth((req) => {
-    const {nextUrl} = req
-    const isLoggedIn = !!req.auth
+//@ts-ignore
+export default auth((req : any) => {
+    const {nextUrl} = req;
+    const isLoggedIn = !!req.auth;
 
     const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
     const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
     const isAuthRoute = authRoutes.includes(nextUrl.pathname)
-    const isAdminRoute = adminRoutes.includes(nextUrl.pathname)
+    //const isAdminRoute = adminRoutes.includes(nextUrl.pathname)
 
     if (isApiAuthRoute) {
         return null
